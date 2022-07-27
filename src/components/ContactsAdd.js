b5fbd24b3833b252081044701c1493e834706107
deliailu27 <54033364+deliailu27@users.noreply.github.com>
2022-07-27 17:23:15 +0100

    update send new contact to server

--- src/components/ContactsAdd.js
+++ src/components/ContactsAdd.js
@@ -1,31 +1,85 @@
-import { useState } from "react"
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function ContactsAdd(props) {
-
   // setContacts and contacts must be passed as props
   // to this component so new contacts can be added to the
   // state
-  const { setContacts, contacts } = props
+  const { setContacts, contacts } = props;
+  const [newContact, setNewContact] = useState({
+    firstName: "",
+    lastName: "",
+    street: "",
+    city: "",
+  });
 
   //TODO: Implement controlled form
   //send POST to json server on form submit
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log("submit event:", event);
+    const updateContacts = [...contacts];
+    updateContacts.push(newContact);
+    setContacts(updateContacts);
+
+    const data = JSON.stringify(contacts);
+    let xhr = new XMLHttpRequest();
+    const url = "http://localhost:4000/contacts";
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(data);
+  }
+
+  function handleChange(event) {
+    const changedID = event.target.id;
+    const Input = event.target.value;
+    const changeContact = { ...newContact };
+    changeContact[changedID] = Input;
+    setNewContact(changeContact);
+  }
+  console.log("New contact:", newContact);
+  console.log("contacts:", contacts);
+
   return (
-    <form className="form-stack contact-form">
+    <form className="form-stack contact-form" onSubmit={handleSubmit}>
       <h2>Create Contact</h2>
 
-      <label htmlFor="firstName">First Name</label>
-      <input id="firstName" name="firstName" type="text" required />
+      <label htmlFor="firstName">First Name:</label>
+      <input
+        id="firstName"
+        name="firstName"
+        type="text"
+        required
+        onChange={handleChange}
+      />
 
       <label htmlFor="lastName">Last Name:</label>
-      <input id="lastName" name="lastName" type="text" required/>
+      <input
+        id="lastName"
+        name="lastName"
+        type="text"
+        required
+        onChange={handleChange}
+      />
 
       <label htmlFor="street">Street:</label>
-      <input id="street" name="street" type="text" required/>
+      <input
+        id="street"
+        name="street"
+        type="text"
+        required
+        onChange={handleChange}
+      />
 
       <label htmlFor="city">City:</label>
-      <input id="city" name="city" type="text" required/>
+      <input
+        id="city"
+        name="city"
+        type="text"
+        required
+        onChange={handleChange}
+      />
 
       <div className="actions-section">
         <button className="button blue" type="submit">
@@ -33,7 +87,7 @@ function ContactsAdd(props) {
         </button>
       </div>
     </form>
-  )
+  );
 }
 
-export default ContactsAdd
+export default ContactsAdd;
